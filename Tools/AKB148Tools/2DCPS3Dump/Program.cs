c8240a2bb7317926269ad80f679576eaed849a47
kurdtkobain <dbz81487@yahoo.com>
2023-06-12 00:57:04 -0400

    2DCPS3Dump to dump 'JPGA' files from 2DC and JPEGA2PNG to convert the dumped 'JPGA' to png 'JPGA' seems to be a normal JPG image with a 8bpp indexed bmp slapped on the end of it. The bmp is essentially an alpha mask

---
+++ Tools/AKB148Tools/2DCPS3Dump/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DCPS3Dump
+{
+    internal class Program
+    {
+
+        private class CH2DTEXI
+        {
+            public byte[] tag = new byte[8]; //CH2DTEXI
+            public int size1;
+            public int diff;
+            public int size2;
+            public int ukn1;
+            public int ukn2;
+            public int ukn3;
+            public int ukn4;
+            public int ukn5;
+            public int ukn6;
+            public int ukn7;
+        };
+        static void Main(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                if (args[0] == "-f")
+                {
+                    dump(args[1], args[2]);
+                }
+                if (args[0] == "-d")
+                {
+                    string[] files = Directory.GetFiles(args[1], "*.2dc");
+                    foreach (string file in files)
+                    {
+                        dump(file, args[2]);
+                    }
+                }
+            }
+        }
+
+        private static void dump(string file, string outDir)
+        {
+            Console.WriteLine($"Processing: {file}");
+            BoyerMooreBinarySearch bm = new BoyerMooreBinarySearch(Encoding.ASCII.GetBytes("CH2DTEXI"));
+            int idx = 0;
+
+            using (BinaryReader br = new BinaryReader(new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read)))
+            {
+                
+                var offsets = bm.GetMatchIndexes(br.BaseStream);
+                foreach (long offset in offsets)
+                {
+                    string outFile;
+                    if (Path.GetFileNameWithoutExtension(file).StartsWith("tc") || Path.GetFileNameWithoutExtension(file).StartsWith("krm"))
+                    {
+                        outFile = $"{outDir}\\{Path.GetFileNameWithoutExtension(file)}\\{Path.GetFileNameWithoutExtension(file)}-{idx}.jpga";
+                    }
+                    else
+                    {
+                        outFile = $"{outDir}\\{Path.GetFileNameWithoutExtension(file).Split('_')[0]}\\{Path.GetFileNameWithoutExtension(file).Split('_')[1]}\\{Path.GetFileNameWithoutExtension(file)}-{idx}.jpga";
+
+                    }
+                    if (File.Exists(outFile))
+                    {
+                        Console.WriteLine($"{outFile} exists skipping.....");
+                        idx++;
+                        continue;
+                    }
+                    br.BaseStream.Position = offset;
+                    CH2DTEXI head = new CH2DTEXI();
+                    head.tag = br.ReadBytes(8);
+                    head.size1 = br.ReadInt32();
+                    head.diff = br.ReadInt32();
+                    head.size2 = br.ReadInt32();
+                    head.ukn1 = br.ReadInt32();
+                    head.ukn2 = br.ReadInt32();
+                    head.ukn3 = br.ReadInt32();
+                    head.ukn4 = br.ReadInt32();
+                    head.ukn5 = br.ReadInt32();
+                    head.ukn6 = br.ReadInt32();
+                    head.ukn7 = br.ReadInt32();
+
+                    byte[] jpgaData = br.ReadBytes(head.size2 + 2);
+                    
+
+                    Console.WriteLine($"Writing file: {outFile}");
+
+                    if (!Directory.Exists(outFile))
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(outFile));
+                    }
+                    File.WriteAllBytes(outFile, jpgaData);
+                    idx++;
+                }
+                
+            }
+
+        }
+    }
+}
---
+++ Tools/AKB148Tools/JPEGA2PNG/Program.cs
@@ -0,0 +1,105 @@
+﻿using KGySoft.Drawing;
+using KGySoft.Drawing.Imaging;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JPEGA2PNG
+{
+    internal class Program
+    {
+
+        public class JPGA
+        {
+            public char[] tag = new char[4];
+            public int numImg;
+            public int jpgSize;
+            public int ukn2;
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                if (args[0] == "-f")
+                {
+                    convert2PNG(args[1]);
+                }
+                if (args[0] == "-d")
+                {
+                    string[] files = Directory.GetFiles(args[1], "*.jpga", SearchOption.AllDirectories);
+                    foreach (string file in files)
+                    {
+                        convert2PNG(file);
+                    }
+                }
+            }
+        }
+        public static void convert2PNG(string file)
+        {
+            //Console.WriteLine($"Processing: {file}");
+            string outFile = Path.GetDirectoryName(file) + "\\" + Path.GetFileNameWithoutExtension(file) + ".png";
+            if (File.Exists(outFile))
+            {
+                Console.WriteLine($"{outFile} exists skipping.....");
+                return;
+            }
+            Bitmap jpg;
+            Bitmap alphaIndexed;
+            using (BinaryReader br = new BinaryReader(File.OpenRead(file)))
+            {
+                JPGA head = new JPGA();
+                head.tag = br.ReadChars(4);
+                if (!head.tag.SequenceEqual(new char[] { 'J', 'P', 'G', 'A' }))
+                {
+                    return;
+                }
+                head.numImg = br.ReadInt32();
+                head.jpgSize = br.ReadInt32();
+                head.ukn2 = br.ReadInt32();
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    byte[] jpgData = br.ReadBytes((int)((head.jpgSize)));
+                    ms.Write(jpgData, 0, jpgData.Length);
+                    jpg = new Bitmap(ms);
+                }
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    while (br.Read() == 0) { }
+                    br.BaseStream.Position -= 1;
+                    int size = (int)(br.BaseStream.Length - br.BaseStream.Position);
+                    byte[] alphaData = br.ReadBytes(size);
+                    ms.Write(alphaData, 0, alphaData.Length);
+                    alphaIndexed = new Bitmap(ms);
+                }
+            }
+
+            mergeImageAndAlphaMaskKGy(jpg, alphaIndexed);
+            Console.WriteLine($"Writing file: {outFile}");
+            jpg.Save(outFile, ImageFormat.Png);
+            jpg.Dispose();
+            alphaIndexed.Dispose();
+
+        }
+
+        public static void mergeImageAndAlphaMaskKGy(Bitmap main, Bitmap alpha)
+        {
+            using (IReadableBitmapData dataAlpha = alpha.GetReadableBitmapData())
+            using (IReadWriteBitmapData dataMain = main.GetReadWriteBitmapData())
+            {
+                Parallel.For(0, dataMain.Height, y =>
+                {
+                    IReadWriteBitmapDataRow rowMain = dataMain[y];
+                    IReadableBitmapDataRow rowAlpha = dataAlpha[y];
+
+                    for (int x = 0; x < dataAlpha.Width; x++)
+                    {
+                        rowMain[x] = new Color32((byte)(255 * rowAlpha[x].GetBrightnessF()), rowMain[x].R, rowMain[x].G, rowMain[x].B);
+                    }
+                });
+            }
+        }
+    }
+}
